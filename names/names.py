d1d76231c8214d9e945183f3190b804b8bca21ca
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-20 16:39:45 -0700

    Add binary_search_tree file

--- names/names.py
+++ names/names.py
@@ -10,10 +10,38 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+"""
+Using original code, it takes 9.9 seconds to find duplicates
+There are 64 duplicates total
+
+Navigate both names directories and make the runtime more efficient to find duplicates.
+runtime = second
+
+Can use one of the data structures we worked on in week's projects
+
+CONSTRAINTS: cannot use list
+
+input: both lists
+output: duplicates only
+
+PLAN: 
+Use data structure from week's project that has best runtime: Binary search tree
+Import it
+name_1
+name_2
+
+Questions: 
+1. Will we need to put both in binary search trees
+
+2. Can we separate the two loops?
+
+ 
+"""
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
+for name_1 in names_1:                #0(n)
+    for name_2 in names_2:            #0(n)
+        if name_1 == name_2:          #
             duplicates.append(name_1)
 
 end_time = time.time()
